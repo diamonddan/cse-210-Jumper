5882af3ba25f1684af3923269de247f6cb3c1789
Heidi <hlmunson@hotmail.com>
2022-02-05 10:23:53 -0700

    methods based on game design

--- Jumper/game/director.py
+++ Jumper/game/director.py
@@ -2,8 +2,8 @@
 # TEMPLATE CLASS
 # Handles user input and the main game loop.
 ################################
-from Game.word_manager import WordManager
-from Game.graphic_manager import GraphicManager
+from game.word_manager import WordManager
+from game.graphic_manager import GraphicManager
 
 class Director:
 
@@ -24,10 +24,16 @@ class Director:
    #Initialize necessary classes and attributes
    def __init__(self):
       """The class constructor
-      Args:
-         self.WordManager: an instance for word_Manager
+   Args:
+      self.WordManager: an instance for word_Manager
       self.GraphicManager: an instance for graphic_manager
-      """
+   """
+      self._graphic = GraphicManager() 
+      self._is_playing = True 
+      self._words= WordManager() 
+      self._terminal_service = TerminalService() 
+
+      
    
 
    #TEMPLATE FUNCTION
@@ -35,49 +41,62 @@ class Director:
    def start_game(self):
       """The class Information Holder
       Args:
-         self (WordManager): an instance of WordManager
+         self (Director): an instance of Director
 
       """
-    
-   
-   #TEMPLATE FUNCTION
-   #returns masked/working word from word_manager
-   def get_word_masked(self):
+      self._words.get_word() #gets word from word manager 
+
+      #display hypthens 
+
+      self._graphic.print_parachute() #display graphic
+
+      while self._is_playing: 
+         self._get_inputs() 
+         self._do_updates() 
+         self._do_outputs() 
+
+   def get_inputs(self): 
       """ gets the masked word from word manager
 
       Args: 
-         self (WordManager): an instance of WordManager
+         self (Director): an instance of Director
       """
 
-      """ gets the number of remaining guesses
+      #ask for letter guess 
+
+      guess =("\nGuess a letter [A-Z]: ") 
+      self._words.is_correct(guess)
+
+   def do_updates(self): 
+      """Updates letter found in word or removes portion of parachute 
 
       Args: 
-         self (WordManager): an instance of WordManager
-      """
+      self (Director): An instance of Director.
+      """ 
+
+      ##looks for letter in word 
+
+      #updates letter in word 
+
+      #removes portion of parachute 
+
+   def do_outputs(self): 
+      """Determines game won or lost. Continues play or ends game. 
+         
+      Args: 
+      self (Director): An instance of Director. 
+      """ 
+
+      #if word is solved - winner game over 
+
+      if self._words.winner(): 
+         self._is_playing = False 
+         print("You Won") 
+
+      #if parachute removed - lost game over 
+      self._is_playing = (self._jumper == "") 
+      if not self._is_playing: 
+         print("Game Over") 
+ 
       
    
-   #TEMPLATE FUNCTION
-   #returns correct word from word manager
-   def get_word(self):
-      """ gets the correct word from word manager
-
-      Args: 
-         self (WordManager): an instance of WordManager
-      """
-
-
-   def get_input(self):
-      """ gets input from the user
-
-      Args: 
-         self (WordManager): an instance of WordManager
-      """
-      
-    
-   def print_remaining_guesses(self):
-      """ prints the remaining guesses to the console
-
-      Args: 
-         self (WordManager): an instance of WordManager
-      """
-      
